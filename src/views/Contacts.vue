7c53f1da75259b29e923904ca40c4a38978113a1
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-10-04 16:42:12 +0200

    add errors catch and print

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -7,6 +7,11 @@ import axios from 'axios';
                 name:'',
                 email:'',
                 message:'',
+                errors:{
+                    name:[],
+                    email:[],
+                    message:[],
+                },
             }
         },
         methods:{
@@ -24,6 +29,20 @@ import axios from 'axios';
                 axios.post('http://127.0.0.1:8000/api/send-email', data)
                     .then(res=>{
                       console.log(res.data);
+                      if(!res.data.success){
+                            this.errors = res.data.errors;
+                        }else{
+                            this.sent = true;
+                            this.errors = {
+                                name:[],
+                                email:[],
+                                message:[],
+                            }
+                        }
+                    })
+                    .catch( er => {
+                      console.log(er.message);
+                      
                     })
               
             },
@@ -38,17 +57,17 @@ import axios from 'axios';
         <div>
             <label for="name">Nome</label>
             <input v-model="name" type="text" id="name" placeholder="Nome">
-            
+            <p class="error">{{ errors.name?.toString() }}</p>
         </div>
         <div>
             <label for="email">Email</label>
             <input v-model="email" type="email" id="email" placeholder="Email">
-            
+            <p class="error">{{ errors.email?.toString() }}</p>
         </div>
         <div>
             <label for="message">Messaggio</label>
             <textarea v-model="message" id="message" rows="10" placeholder="Messaggio"></textarea>
-            
+            <p class="error">{{ errors.message?.toString() }}</p>
         </div>
         <button type="submit">Invia</button>
         <button type="reset">Annulla</button>
@@ -66,5 +85,10 @@ input,textarea{
     width: 80%;
     border-radius: 5px;
     padding: 10px
+}
+.error{
+    color: yellow;
+    font-size: .9rem;
+    margin: 0;
 }
 </style>
